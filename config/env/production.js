119ef387cf8a553bb0307bc47c89e5a7a5425990
Yan <yanc.cortes@outlook.com>
2023-04-21 21:35:19 -0500

    Cambia production

--- config/env/production.js
+++ config/env/production.js
@@ -7,7 +7,7 @@
  * is only used in your production environment, i.e. when you lift your app using:
  *
  * ```
- * NODE_ENV=production node app
+ *+ NODE_ENV=production node app
  * ```
  *
  * > If you're using git as a version control solution for your Sails app,
@@ -47,8 +47,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
+      adapter: 'sails-mongo',
+      url: 'mongodb://ynccortes:hwh1eGrMlAPxjbyQ@ac-z2oxmfu-shard-00-00.fycmynb.mongodb.net:27017,ac-z2oxmfu-shard-00-01.fycmynb.mongodb.net:27017,ac-z2oxmfu-shard-00-02.fycmynb.mongodb.net:27017/?ssl=true&replicaSet=atlas-q5phs5-shard-0&authSource=admin&retryWrites=true&w=majority',
+    
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
